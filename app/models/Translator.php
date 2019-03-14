5ad166155d64db44b3d908a53e2cf086084059ff
Matej Soroka <hi@matejsoroka.com>
2019-03-14 21:06:37 +0100

    Translator model implementation

---
+++ app/models/Translator.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Models;
+
+class Translator {
+
+    private $input;
+    private $syllable = "ay";
+    private $delimiter = "-";
+    private $consonants = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "X", "Z"];
+
+    /**
+     * @param string $input english word
+     */
+    public function setInput(string $input)
+    {
+        $this->input = $input;
+    }
+
+    /**
+     * @return string Get english word
+     */
+    public function getInput() : string
+    {
+        return $this->input;
+    }
+
+	/**
+	 * @return string Translated english word into pig latin
+	 */
+	public function translate() : string
+    {
+        $input = $this->getInput();
+        $index = $this->getFirstVowelIndex($input);
+        if ($index) {
+            $suffix = substr($input, 0, $index);
+            $prefix = substr($input, $index, strlen($input));
+            $input = $prefix.$this->delimiter.$suffix.$this->syllable;
+        } else {
+            $input .= "'" . $this->getRandomConsonant() . $this->syllable;
+        }
+		return $input;
+    }
+
+	/**
+	 * @return string Random consonant
+	 */
+	private function getRandomConsonant() : string
+	{
+		return strtolower($this->consonants[rand(0, count($this->consonants) - 1)]);
+	}
+
+	/**
+	 * @param $word string english word
+	 * @return int position of first vowel
+	 */
+	private function getFirstVowelIndex(string $word) : int
+    {
+        $i = 0;
+        foreach (str_split($word) as $char) {
+            if (in_array(strtoupper($char), $this->consonants)) {
+                $i++;
+            } else {
+                break;
+            }
+        }
+        return $i;
+    }
+
+}
